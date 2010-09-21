59ea1dfe16f06493862dfb3d19844fe13188fbc9
says0528 <says0528@c4e277b9-bfaf-bb29-3575-63098d51153f>
2010-09-21 19:53:06 +0000

    The concat functionality does not work the same in mySQL as it does in Oracle, therefore the response query that did not return history was not working properly for mySQL.  The query has been modified to work for both Oracle and mySql.  Also, indexes were added to the BannerResponse table to improve the speed of the query response.

--- branches/dev_curriculum/JavaSource/org/unitime/banner/model/dao/BannerResponseDAO.java
+++ branches/dev_curriculum/JavaSource/org/unitime/banner/model/dao/BannerResponseDAO.java
@@ -237,7 +237,7 @@ public class BannerResponseDAO extends BaseBannerResponseDAO {
             }
             
             if (!showHistory) {
-            	whereHql += " and rp.queueId = (select max(queueId) from BannerResponse rp2 where concat(concat(rp2.termCode,rp2.crn) ,rp2.xlstGroup)= concat(concat(rp.termCode,rp.crn),rp.xlstGroup))";
+            	whereHql += " and rp.queueId = (select max(queueId) from BannerResponse rp3 where rp3.termCode = rp.termCode and rp3.crn = rp.crn and ((rp3.xlstGroup is null and rp.xlstGroup is null) or (rp3.xlstGroup = rp.xlstGroup))) ";
             }
             			
             String hql = "select rp " +
