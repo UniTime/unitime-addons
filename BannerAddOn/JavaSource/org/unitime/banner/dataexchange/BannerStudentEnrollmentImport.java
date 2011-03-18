d3d723f082b763b57c2ed2c7cb8909a40fd1d11e
Tomáš Müller <muller@unitime.org>
2011-03-18 20:37:28 +0000

    Banner Student Enrollment Import  - update class / course enrollments -- ensure that there is a Hibernate session open for the update (the commitTransaction() called a few lines above the update leaves the session closed)

--- BannerAddOn/JavaSource/org/unitime/banner/dataexchange/BannerStudentEnrollmentImport.java
+++ BannerAddOn/JavaSource/org/unitime/banner/dataexchange/BannerStudentEnrollmentImport.java
@@ -42,6 +42,7 @@ import org.unitime.timetable.model.StudentClassEnrollment;
 import org.unitime.timetable.model.StudentSectioningQueue;
 import org.unitime.timetable.model.TimetableManager;
 import org.unitime.timetable.model.dao.StudentDAO;
+import org.unitime.timetable.model.dao._RootDAO;
 import org.unitime.timetable.test.UpdateExamConflicts;
 
 /**
@@ -232,13 +233,16 @@ public class BannerStudentEnrollmentImport extends BaseImport {
         }
 
         if (session != null && "true".equals(ApplicationProperties.getProperty("tmtbl.data.import.studentEnrl.class.updateEnrollments","false"))){
+        	org.hibernate.Session hibSession = new _RootDAO().createNewSession();
             try {
                 info("  Updating class enrollments...");
-                Class_.updateClassEnrollmentForSession(session, getHibSession());
+                Class_.updateClassEnrollmentForSession(session, hibSession);
                 info("  Updating course offering enrollments...");
-                CourseOffering.updateCourseOfferingEnrollmentForSession(session, getHibSession());
+                CourseOffering.updateCourseOfferingEnrollmentForSession(session, hibSession);
             } catch (Exception e) {
                 fatal("Exception: " + e.getMessage(), e);
+            } finally {
+            	hibSession.close();
             }      	
         }
         
