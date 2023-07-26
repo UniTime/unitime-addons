1da3ea07c859142fac28a025a1aab27ac33d7b0b
tomas-muller <muller@unitime.org>
2023-07-26 21:05:41 +0200

    Course Finder: Filter
    - Course Finder dialog on the Scheduling Assistant, Course Requests, and Advisor Course Recommendations pages improved
    - added Filter section, which allows for filtering courses by
      - class start/end date (given course must have an enrolment that does not start before the given From date, or lasts past the To date)
      - min/max credit
      - instructor name (given course must have a class with matching instructor name)
    - notes
      - classes that do not display instructors are not considered by the instructor filter
      - classes that are not enabled for student scheduling are ignored (both instrutor and class dates filters)
      - the filter is ONLY used for the list of courses, it does not guarantee that the student gets a class with the given instructor or within the given dates

--- BannerAddOn/JavaSource/org/unitime/banner/onlinesectioning/BannerListCourseOfferings.java
+++ BannerAddOn/JavaSource/org/unitime/banner/onlinesectioning/BannerListCourseOfferings.java
@@ -82,7 +82,7 @@ public class BannerListCourseOfferings extends ListCourseOfferings {
 					Long courseId = (Long)courseClassId[0];
 					Long sectionId = (Long)courseClassId[1];
 					XCourse course = server.getCourse(courseId);
-					if (course != null && (iMatcher == null || iMatcher.match(course))) {
+					if (course != null && (iMatcher == null || iMatcher.match(course)) && matchFilter(server, iFilter, course)) {
 						XOffering offering = server.getOffering(course.getOfferingId());
 						XSection section = (offering == null ? null : offering.getSection(sectionId));
 						if (section != null) {
@@ -142,7 +142,7 @@ public class BannerListCourseOfferings extends ListCourseOfferings {
 		for (XCourseId id: server.findCourses(iQuery, iLimit, iMatcher)) {
 			if (courses != null && courses.containsKey(id.getCourseId())) continue;
 			XCourse course = server.getCourse(id.getCourseId());
-			if (course != null)
+			if (course != null && matchFilter(server, iFilter, course))
 				ret.add(convert(course, server));
 			if (iLimit != null && iLimit > 0 && ret.size() == iLimit) break;
 		}
