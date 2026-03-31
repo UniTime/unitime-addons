57666490d9096d9adc14fa86366ded8e11f6e16b
says0528 <says@unitime.org>
2026-03-31 09:19:05 -0400

    Update BannerExternalClassNameHelper.java
    Added handling for null CRN to fix null error in string creation.

--- BannerAddOn/JavaSource/org/unitime/banner/util/BannerExternalClassNameHelper.java
+++ BannerAddOn/JavaSource/org/unitime/banner/util/BannerExternalClassNameHelper.java
@@ -60,7 +60,7 @@ public class BannerExternalClassNameHelper extends DefaultExternalClassNameHelpe
 		} else {
 			BannerSection bs = BannerSection.findBannerSectionForClassAndCourseOfferingCacheable(clazz, courseOffering, BannerSectionDAO.getInstance().getSession());
 			if (bs != null){
-				return courseOffering.getCourseName()+" "+clazz.getItypeDesc().trim()+" "+ bs.getCrn().toString();
+				return courseOffering.getCourseName()+" "+clazz.getItypeDesc().trim()+" "+ (bs.getCrn() == null ?" ":(bs.getCrn().toString()));
 			} else {
 				return super.getClassLabel(clazz, courseOffering);
 			}
@@ -77,7 +77,7 @@ public class BannerExternalClassNameHelper extends DefaultExternalClassNameHelpe
 		} else {
 			BannerSection bs = BannerSection.findBannerSectionForClassAndCourseOfferingCacheable(clazz, courseOffering, BannerSectionDAO.getInstance().getSession());
 	    	if (bs != null) {
-	    		return courseOffering.getCourseNameWithTitle()+" "+clazz.getItypeDesc().trim()+" "+bs.getCrn().toString();
+	    		return courseOffering.getCourseNameWithTitle()+" "+clazz.getItypeDesc().trim()+" "+(bs.getCrn() == null?"":(bs.getCrn().toString()));
 	    	} else {
 	    		return super.getClassLabelWithTitle(clazz, courseOffering);
 	    	}
@@ -94,7 +94,7 @@ public class BannerExternalClassNameHelper extends DefaultExternalClassNameHelpe
 		} else {
 			BannerSection bs = BannerSection.findBannerSectionForClassAndCourseOfferingCacheable(clazz, courseOffering, BannerSectionDAO.getInstance().getSession());
 			if (bs != null){
-				return bs.getCrn().toString() + '-' + bs.getSectionIndex() + (courseOffering.getInstructionalOffering().getCourseOfferings().size() > 1?"*":"");
+				return (bs.getCrn() == null?"":(bs.getCrn().toString())) + '-' + bs.getSectionIndex() == null?"":bs.getSectionIndex() + (courseOffering.getInstructionalOffering().getCourseOfferings().size() > 1?"*":"");
 			} else {
 				return(clazz.getClassSuffix());
 			}
@@ -111,7 +111,7 @@ public class BannerExternalClassNameHelper extends DefaultExternalClassNameHelpe
 		} else {
 			BannerSection bs = BannerSection.findBannerSectionForClassAndCourseOfferingCacheable(clazz, courseOffering, BannerSectionDAO.getInstance().getSession());
 			if (bs != null) {
-				return bs.getCrn().toString();
+				return (bs.getCrn() == null?"":(bs.getCrn().toString()));
 			} else {
 				return(clazz.getExternalUniqueId());
 			}
@@ -286,7 +286,7 @@ public class BannerExternalClassNameHelper extends DefaultExternalClassNameHelpe
 			} else {
 				BannerSectionCache bs = getBannerSectionCache(clazz, courseOffering);
 				if (bs != null) {
-					return courseOffering.getCourseName()+" "+clazz.getItypeDesc().trim()+" "+ bs.getCrn().toString();
+					return courseOffering.getCourseName()+" "+clazz.getItypeDesc().trim()+" "+(bs.getCrn() == null?"":(bs.getCrn().toString()));
 				} else {
 					return super.getClassLabel(clazz, courseOffering);
 				}
@@ -300,7 +300,7 @@ public class BannerExternalClassNameHelper extends DefaultExternalClassNameHelpe
 			} else {
 				BannerSectionCache bs = getBannerSectionCache(clazz, courseOffering);
 		    	if (bs != null) {
-		    		return courseOffering.getCourseNameWithTitle()+" "+clazz.getItypeDesc().trim()+" "+bs.getCrn().toString();
+		    		return courseOffering.getCourseNameWithTitle()+" "+clazz.getItypeDesc().trim()+" "+(bs.getCrn() == null?"":(bs.getCrn().toString()));
 		    	} else {
 		    		return super.getClassLabelWithTitle(clazz, courseOffering);
 		    	}
@@ -314,7 +314,7 @@ public class BannerExternalClassNameHelper extends DefaultExternalClassNameHelpe
 			} else {
 				BannerSectionCache bs = getBannerSectionCache(clazz, courseOffering);
 				if (bs != null){
-					return bs.getCrn().toString() + '-' + bs.getSectionIndex() + (courseOffering.getInstructionalOffering().getCourseOfferings().size() > 1?"*":"");
+					return (bs.getCrn() == null?"":(bs.getCrn().toString())) + '-' + bs.getSectionIndex() == null?"":bs.getSectionIndex() + (courseOffering.getInstructionalOffering().getCourseOfferings().size() > 1?"*":"");
 				} else {
 					Debug.warning("No banner section cached for " + courseOffering.getCourseName() + " " + clazz.getItypeDesc().trim() + " " + clazz.getSectionNumberString());
 					return clazz.getClassSuffix();
@@ -329,7 +329,7 @@ public class BannerExternalClassNameHelper extends DefaultExternalClassNameHelpe
 			} else {
 				BannerSectionCache bs = getBannerSectionCache(clazz, courseOffering);
 				if (bs != null) {
-					return bs.getCrn().toString();
+					return (bs.getCrn() == null?"":(bs.getCrn().toString()));
 				} else {
 					return clazz.getExternalUniqueId();
 				}
